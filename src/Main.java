f33b9c928f4571912ea229d3143bc825bd97b919
Cyberbara <125134773+cyberbara@users.noreply.github.com>
2025-11-19 12:43:43 +0300

    1.2_upd

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // 1.1
+        java.util.ArrayList<String> s = new java.util.ArrayList<>();
+        s.add("a"); s.add("b"); s.add("a");
+        System.out.println(Alghoritms.removeDups(s));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // 1.2
+        Integer[] arr = {1, 5, 9};
+        System.out.println(Alghoritms.lSearch(arr, 5));
     }
 }
